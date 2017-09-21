a597306cd0aa0f023419f4b0a9cf1d398bb553e6
Faisaljaved <faisaljaved06@gmail.com>
2017-09-21 15:33:01 +0500

    next

--- app/src/main/java/com/example/faisal/a1stapp/MainActivity.java
+++ app/src/main/java/com/example/faisal/a1stapp/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.faisal.a1stapp;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,10 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Fragment1 fragment = new Fragment1();
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.,fragment);
-        fragmentTransaction.commit();
+        
     }
 }
